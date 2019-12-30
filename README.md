519230938aa8e2a6fb4c6b01a151f93f7a3932df
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2019-12-30 00:53:44 -0500

    Basic backend structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # blog-monorepo
 Back and multiple fronts in a folder project
+
+Based on tutorial: https://www.digitalocean.com/community/tutorials/how-to-build-a-blog-with-nest-js-mongodb-and-vue-js
--- blog-backend/src/app.module.ts
+++ blog-backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@blog-cluster-xmwwh.gcp.mongodb.net/test?retryWrites=true&w=majority`,
+      { useNewUrlParser: true }
+    ),
+    BlogModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
