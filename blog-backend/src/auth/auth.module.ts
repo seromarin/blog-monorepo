c2ad14893722e57348b9273ded3fd6158e52e7d8
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-01-02 17:29:23 -0500

    Full passport jwt auth

--- blog-backend/src/auth/auth.module.ts
+++ blog-backend/src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
-import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { jwtConstants } from './constant';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy]
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '30m' },
+    }),
+    UsersModule,
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- blog-backend/src/auth/auth.service.ts
+++ blog-backend/src/auth/auth.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-
   constructor(
-    private readonly userService: UsersService
-  ) { }
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.userService.findOneUserByUsername(username);
-    if (user && user.password === password) {
-      const { pass, ...result } = user;
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOneUserByUsername(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
       return result;
     }
     return null;
   }
 
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
