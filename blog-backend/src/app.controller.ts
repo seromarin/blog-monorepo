58cbaa94506c1696b5c89df16f745e3a35a7d6cd
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-01-02 16:53:57 -0500

    Basic passport auth local

--- blog-backend/src/app.controller.ts
+++ blog-backend/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   entrypointApi(): string {
     return this.appService.entrypointApi();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
